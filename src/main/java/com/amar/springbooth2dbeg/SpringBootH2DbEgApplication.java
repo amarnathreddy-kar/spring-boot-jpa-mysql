07934255d2fb9ac7c46c4c1765201b0530c59d1d
amarnath_k <amarnathredddy.kar@gmail.com>
2022-08-28 18:19:17 +0530

    add password encryption

--- src/main/java/com/amar/springbooth2dbeg/SpringBootH2DbEgApplication.java
+++ src/main/java/com/amar/springbooth2dbeg/SpringBootH2DbEgApplication.java
@@ -1,9 +1,11 @@
 package com.amar.springbooth2dbeg;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SpringBootH2DbEgApplication {
 
 	public static void main(String[] args) {
